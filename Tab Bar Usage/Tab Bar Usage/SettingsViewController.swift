d39b932f4bdac56cb1cf3a1b55ecce6b85ee855c
erkutahmet <90329328+erkutahmet@users.noreply.github.com>
2023-10-13 01:01:29 +0300

    created other screens and navigation controller

---
+++ Tab Bar Usage/Tab Bar Usage/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  Tab Bar Usage
+//
+//  Created by Ahmet Erkut on 13.10.2023.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
