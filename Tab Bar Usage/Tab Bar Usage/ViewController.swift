76ac55e1de9e1e82a1a6ade464d6f44a304fa976
erkutahmet <90329328+erkutahmet@users.noreply.github.com>
2023-10-13 01:41:02 +0300

    Tab Bar, Badge, Navigation Controller

--- Tab Bar Usage/Tab Bar Usage/ViewController.swift
+++ Tab Bar Usage/Tab Bar Usage/ViewController.swift
@@ -11,9 +11,34 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let tabItems = tabBarController?.tabBar.items {
+            let homescreenItem = tabItems[0]
+            let settingsItem = tabItems[1]
+            homescreenItem.badgeValue = "3"
+            settingsItem.badgeValue = "New"
+        }
+        
+        let appearance = UITabBarAppearance()
+        appearance.backgroundColor = UIColor.systemIndigo
+        
+        changeColor(itemAppearance: appearance.stackedLayoutAppearance)
+        changeColor(itemAppearance: appearance.inlineLayoutAppearance)
+        changeColor(itemAppearance: appearance.compactInlineLayoutAppearance)
+        
+        tabBarController?.tabBar.standardAppearance = appearance
+        tabBarController?.tabBar.scrollEdgeAppearance = appearance
     }
-
-
+    
+    func changeColor(itemAppearance: UITabBarItemAppearance) {
+        // selected status
+        itemAppearance.selected.iconColor = UIColor.systemOrange
+        itemAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor.systemOrange]
+        itemAppearance.selected.badgeBackgroundColor = UIColor.systemMint
+        // non-selected status
+        itemAppearance.normal.iconColor = UIColor.white
+        itemAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.white]
+        itemAppearance.normal.badgeBackgroundColor = UIColor.lightGray
+    }
 }
 
